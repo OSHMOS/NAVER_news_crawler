3ae75cba8ac04bcde07c2942b4955830bed47532
SEUNGHYUN OH <seunghyunoh@SEUNGHYUNui-MacBookAir.local>
2022-12-14 21:54:19 +0900

    12.14 search_scrapper 1차 완

---
+++ scrap_naver.py
@@ -0,0 +1,6 @@
+
+
+LIMIT = 0
+
+while True:
+    url = f'https://search.naver.com/search.naver?where=news&sm=tab_pge&query=%EB%B6%81%ED%95%9C%EC%9D%B8%EA%B6%8C%EC%A0%95%EB%B3%B4%EC%84%BC%ED%84%B0&sort=1&photo=0&field=0&pd=0&ds=&de=&mynews=0&office_type=0&office_section_code=0&news_office_checked=&nso=so:dd,p:all,a:all&start={LIMIT}1'
