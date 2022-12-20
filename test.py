b0972a5118c6aceaf062194fd2c1d3d21494409c
SEUNGHYUN OH <seunghyunoh@SEUNGHYUNui-MacBookAir.local>
2022-12-20 20:37:27 +0900

    12.20 add press, date (test)

--- test.py
+++ test.py
@@ -1,12 +1,28 @@
+import csv
 import requests
+from datetime import datetime
 from bs4 import BeautifulSoup
-from openpyxl import Workbook
 
-url = 'https://www.google.co.kr/search?q=%22%EB%B6%81%ED%95%9C%EC%9D%B8%EA%B6%8C%EC%A0%95%EB%B3%B4%EC%84%BC%ED%84%B0%22&tbm=nws&sxsrf=ALiCzsaK4d-_mEpqj5QdWCu1nDvHYY_XDA:1670611316965&ei=dIGTY8K5OsOgoASWy6iQAw&start=230'
+
+csv_file = open('().csv', 'w')
+csv_writer = csv.writer(csv_file)
+csv_writer.writerow(['뉴스 제목', '뉴스 링크', '언론사', '날짜'])
+
+url = 'https://search.naver.com/search.naver?where=news&sm=tab_pge&query=%22%EC%9B%94%EB%93%9C%EC%BB%B5%22&sort=1&photo=0&field=0&pd=0&ds=&de=&mynews=0&office_type=0&office_section_code=0&news_office_checked=&nso=so:dd,p:all,a:all&start=1'
 rep = requests.get(url)
 test_page = rep.text
 
 soup = BeautifulSoup(test_page, 'html.parser')
 
-items = soup.select('.WlydOe')
-print(items)
+news_titles = soup.select('a.news_tit')
+info_groups = soup.select('a.info.press')
+info_dates = soup.select('span.info')
+for news_title, info_group, info_date in zip(news_titles, info_groups, info_dates):
+    title = news_title.get_text()
+    link = news_title.get('href')
+    press = info_group.get_text()
+    date = info_date.get_text()
+    if '분 전' in date:
+        date = datetime.now().date()
+    csv_writer.writerow([title, link, press, date])
+csv_file.close()
